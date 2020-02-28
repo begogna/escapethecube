40f235463f0ce248d727c6569bbfa5dbb417f6c4
Begoña Pérez Rodríguez <begmeil@gmail.com>
2020-02-28 18:34:45 +0100

    Adding feedbacks fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA1tbvWdB3GhCTnmekJiipFFYieDxuh8Pc',
+    authDomain: 'escapethecube-fb.firebaseapp.com',
+    databaseURL: 'https://escapethecube-fb.firebaseio.com',
+    projectId: 'escapethecube-fb',
+    storageBucket: 'escapethecube-fb.appspot.com',
+    messagingSenderId: '738506709251',
+    appId: '1:738506709251:web:0cc986fb525fe00aa4f136',
+  },
 };
